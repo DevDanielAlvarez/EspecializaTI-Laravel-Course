72dcc81c4bdf535f63a4ed50f5c9b25ea3496960
DevDanielAlvarez <skyrimdaniel11@gmail.com>
2024-01-02 22:14:16 -0300

    install laravel

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+    /**
+     * return all supports
+     */
+    public function index(){
+        return view();
+    }
+}
