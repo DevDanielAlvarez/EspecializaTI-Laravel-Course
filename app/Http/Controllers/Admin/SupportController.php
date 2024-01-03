79949704a2470cdf68ceff8dcf9655e0f023f20e
DevDanielAlvarez <skyrimdaniel11@gmail.com>
2024-01-03 11:33:09 -0300

    feat(support): add index view for supports

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
+    protected string $viewPath = "admin/support/";
     /**
      * return all supports
      */
-    public function index(){
-        return view();
+    public function index():View{
+
+        return view($this->viewPath.'index');
+        
     }
 }
